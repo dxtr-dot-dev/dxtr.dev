5076ddfb0f1b375db8d68938f15fbebc886a3542
Pedro Moreira <pedsmoreira@gmail.com>
2023-02-18 16:04:55 -0300

    Back to auto adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,19 +8,8 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: false,
+		adapter: adapter(),
 
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
-		})
-	},
-
-	kit: {
 		alias: {
 			'$components/*': 'src/components/*',
 			'$types/*': 'src/types/*'
