7e2727021ea9719ba127bbf6c1fae0b3d7c15c95
Pedro Moreira <pedsmoreira@gmail.com>
2023-03-08 16:43:32 -0300

    Improve meta tags

--- src/lib/components/head.svelte
+++ src/lib/components/head.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-
 	export let title = 'DXTR.DEV';
 	export let description = 'Tools and apps for a delightful developer experience';
 	export let image = 'dxtr.dev/dxtr.dev-cover.jpg';
+
+	$: ogImage = `https://dxtr.dev/${image}`;
 </script>
 
 <svelte:head>
 	<title>{title}</title>
 	<meta name="description" content={description} />
-	<meta property="og:image" content={`https://dxtr.dev/${image}`} />
+
+	<meta property="og:site_name" content="DXTR.DEV" />
+	<meta property="og:title" content={title} />
+	<meta property="og:image" content={ogImage} />
+	<meta property="og:image:width" content="1640" />
+	<meta property="og:image:height" content="924" />
+
+	<meta name="twitter:site" content="@dxtr_dot_dev" />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:image" content={ogImage} />
 </svelte:head>
